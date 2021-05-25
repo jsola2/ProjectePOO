455c711c4d473117579718765c0da05d5a54b74b
Jaime Sola <j.sola2@gmail.com>
2021-05-25 11:01:04 +0200

    Codi base per a l'equip

---
+++ src/GestorDades.java
@@ -0,0 +1,18 @@
+
+
+public class GestorDades {
+
+    public Object carregarDades;
+
+    public Object getCarregarDades() {
+        return carregarDades;
+    }
+
+    public Object consultar;
+    public Object afegir;
+
+    public Object esborrar;
+    public Object sortir;
+}
+
+
---
+++ src/ProjectePOO.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProjectePOO {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        GestorDades gestor = new GestorDades();
+        int menu = in.nextInt();
+        switch (menu){
+            /*case 1 : gestor.carregarDades;
+            case 2 : gestor.consultar;
+            case 3 : gestor.afegir;
+            case 4 : gestor.esborrar;
+            case 0 : gestor.sortir;*/
+        }
+    }
+
+}
