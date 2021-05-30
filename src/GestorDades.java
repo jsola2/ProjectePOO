173322c4afe0554ac243240ccd6eb0e21c2c01ce
Jaime Solà <j.sola2@sapalomera.cat>
2021-05-30 11:56:31 +0200

    Actualitzant Afegir Treballador

--- src/GestorDades.java
+++ src/GestorDades.java
@@ -1,12 +1,14 @@
+import model.LlistaTreballadors;
 import model.Treballador;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class GestorDades {
 
 
-    /*public Object carregarDades ();
+    public Object carregarDades ();
 
 
     public Object getCarregarDades() {
@@ -16,14 +18,39 @@ public class GestorDades {
     public Object consultar;
     public void afegir () {
 
+         Scanner in = new Scanner(System.in);
+         int menuAfegir = in.nextInt();
 
+         LlistaTreballadors a = new LlistaTreballadors();
+         switch (menuAfegir){
+
+              case 1 : a.afegirTreballador();break;
+              case 2 :
+              case 3 :
+              case 0 :
             }
         }
 
-    }
+
+
 
     public void esborrar () {
+         Scanner in = new Scanner(System.in);
+         int menuEsborrar = in.nextInt();
+         LlistaTreballadors e = new LlistaTreballadors();
+         switch (menuEsborrar) {
+              case 1:
+                   e.esborrarTreballador();
+              case 2:
+              case 3:
+         }
+    }
+    public void sortir () {
 
-    public Object sortir;*/
+          Scanner in = new Scanner(System.in);
+          int seleccio = in.nextInt();
+          if (seleccio == 0){
 
+          }
+         }
      }
