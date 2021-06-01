71e0129eb69b98b649233b2e96b5af0f66b03d67
Bangal Camara <bangalcamara2002@gmail.com>
2021-06-01 18:11:09 +0200

    Hem creat la clase presona per despres utilitzar el seus atributs gracies a la herencia

---
+++ src/UF4/model/Persona.java
@@ -0,0 +1,16 @@
+package UF4.model;
+
+public class Persona {
+    protected String nom;
+    protected String cognom;
+    protected String dni;
+
+
+    public Persona(String nom, String cognom, String dni) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.dni = dni;
+    }
+
+    public Persona() { }
+}
